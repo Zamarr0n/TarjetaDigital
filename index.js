1dc0b9858c4e03cc1737ca33959f070abba1e668
Zamarron <emilio.zamarrons@gmail.com>
2024-11-10 22:25:05 -0600

    final touches of the presentation card

---
+++ index.js
@@ -0,0 +1,7 @@
+const whatsapp = document.getElementById('whatsapp');
+
+// https://wa.me/528714243572
+whatsapp.addEventListener('click', () => {
+const url = 'https://wa.me/528714243572';
+window.open(url, '_blank');
+});
