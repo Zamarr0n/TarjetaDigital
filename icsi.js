eb52c5f0b2e723cffe184af20e5430badcca242f
Zamarron <emilio.zamarrons@gmail.com>
2025-03-02 22:10:47 -0600

    adding touches

--- icsi.js
+++ icsi.js
@@ -1,16 +1,17 @@
 const whatsapp_Gabriel_Vega = document.getElementById('whatsapp_Gabriel_Vega');
-const web_icsi = document.getElementById('icsi-web');
 
 whatsapp_Gabriel_Vega.addEventListener('click', () => {
-const url = 'https://wa.me/528711147823';
-window.open(url, '_blank');
-});
+    const url = 'https://wa.me/528711147823';
+    window.open(url, '_blank');
+});    
 
 
 
-web_icsi.addEventListener('click', () => {
-    window.open('https://icsingenieria.mx/index.html', '_blank');
-});
+
+
+
+
+
 
 
 
